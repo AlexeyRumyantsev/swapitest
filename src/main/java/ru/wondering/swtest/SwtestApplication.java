5842abe64c00d3d3cae8a21b1f12521c28f0912c
AlexeyRumyantsev <lexis@wondering.ru>
2023-11-29 23:37:56 +0800

    Init project structure: 1. Add gradle wrapper and configuration 2. Add lombok configuration 3. Add SpringBoot application

---
+++ src/main/java/ru/wondering/swtest/SwtestApplication.java
@@ -0,0 +1,11 @@
+package ru.wondering.swtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwtestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SwtestApplication.class, args);
+    }
+}
